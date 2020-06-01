4640e38fc1985f9ca3d03f9ac74366f5c0559192
Yongen Jonavan Voo <s10196680@connect.np.edu.sg>
2020-06-01 14:26:28 +0800

    Prac 5

--- app/src/main/java/sg/edu/np/mad/mad_recyclerview/recyclerViewAdapter.java
+++ app/src/main/java/sg/edu/np/mad/mad_recyclerview/recyclerViewAdapter.java
@@ -1,5 +1,7 @@
 package sg.edu.np.mad.mad_recyclerview;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +19,33 @@ public class recyclerViewAdapter extends RecyclerView.Adapter<recyclerViewHolder
         View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_layout,parent,false);
         return new recyclerViewHolder(item);
     }
-    public void onBindViewHolder(recyclerViewHolder holder,int position){
+    public void onBindViewHolder(final recyclerViewHolder holder, final int position){
         Task s = data.get(position);
         holder.txt.setText(s.getTask());
         holder.checkbox.setChecked(s.isCheckbox());
+        holder.txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("Delete");
+                builder.setMessage("Are you sure you want to delete\n" + holder.txt.getText().toString() + "?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        data.remove(position);
+                        notifyDataSetChanged();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
     }
     public int getItemCount(){
         return data.size();
